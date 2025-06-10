a60c314b1e5f272aeaf383977a2ffcc72913eca5
Hungvu0712 <122802824+Hungvu0712@users.noreply.github.com>
2025-06-11 00:55:09 +0700

    fix asgin role

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -47,6 +47,7 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], func
 });
 
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
