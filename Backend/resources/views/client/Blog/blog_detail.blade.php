2d114813417853c49daf68f8ce55b3c8343715c6
Hungvu0712 <vu7231726@gmail.com>
2025-06-26 16:32:10 +0700

    fix css blog detail

--- Backend/resources/views/client/Blog/blog_detail.blade.php
+++ Backend/resources/views/client/Blog/blog_detail.blade.php
@@ -11,55 +11,8 @@
 @endsection
 
 @endsection
-{{-- @include('client.partials.banner') --}}
-@section('content')
-<section class="popup_search_sec">
-    <div class="popup_search_overlay"></div>
-    <div class="pop_search_background">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 col-md-6">
-                    <div class="popup_logo">
-                        <a href="index.html"><img src="images/logo2.png" alt="Ulina"></a>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-6">
-                    <a href="javascript:void(0);" id="search_Closer" class="search_Closer"></a>
-                </div>
-            </div>
-        </div>
-        <div class="middle_search">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 text-center">
-                        <div class="popup_search_form">
-                            <form method="get" action="#">
-                                <input type="search" name="s" id="s"
-                                    placeholder="Type Words and Hit Enter">
-                                <button type="submit"><i class="fa-solid fa-search"></i></button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- END: Search Popup Section -->
 
-<!-- BEGIN: Page Banner Section -->
-{{-- <section class="pageBannerSection singleBlogPageBanner">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 offset-lg-3">
-                    <div class="pageBannerContent text-center">
-                        <img src="{{ asset('public/client/images/slider/3.jpg') }}" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> --}}
-<!-- END: Page Banner Section -->
+@section('content')
 
 <!-- BEGIN: Blog Page Section -->
 <section class="blogDetailsPageSection mt-5">
@@ -69,7 +22,7 @@
             @if ($post)
                 <div class="col-lg-10 offset-lg-1">
                     <div class="blogDetailsThumb">
-                        {{-- <img src="" style="width: 500" height="300" alt="Blog Details" /> --}}
+                        
                     </div>
                     <div class="blogDetailsContentArea">
                         <div class="bi01Meta clearfix">
@@ -80,7 +33,7 @@
                                 </a></span>
                         </div>
                         <h2 class="postTitle">{{ $post->excerpt }}</h2>
-                        <div class="blogDetailscontent">
+                        <div>
                             <p>
                                 {!! $post->content !!}
                             </p>
